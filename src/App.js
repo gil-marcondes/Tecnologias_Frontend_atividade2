1382c26a2e017043364812f9a7c9dc4e0c605a32
gil-marcondes <73506561+gil-marcondes@users.noreply.github.com>
2020-11-22 22:29:08 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import UsuarioEntrada from './UsuarioEntrada';
+import './App.css';
+
+class App extends Component{
+  render(){
+  return (
+  <div className="App">
+  <h1>Tecnologias Front-end: Atividade2</h1>
+        <UsuarioEntrada/>
+		
+      </div>
+	  );
+}
+}
+
+export default App;
